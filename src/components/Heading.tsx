5c0923b6e02b9348374499e2685e4ac3e401af23
ktsn_ud <igcz025221@gmail.com>
2025-06-02 20:52:34 +0900

    fix: Headingコンポーネントのマージンを修正

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -2,7 +2,7 @@ import type { ContentItemType } from '../types/contents';
 
 export function Heading({ content }: { content: ContentItemType }) {
     return (
-        <h2 id={content.link} className='block relative mb-10'>
+        <h2 id={content.link} className='block relative my-10'>
                 {/* <div className='absolute font-extrabold text-5xl text-gray-300 pointer-events-none'>{content.label}</div>
                 <div className='absolute font-extrabold text-4xl top-7 left-8 text-gray-800 tracking-wider'>{content.label_ja}</div> */}
 
