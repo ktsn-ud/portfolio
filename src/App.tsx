ab399ba5f28f484194ca61ed338f5558e995ea8e
ktsn_ud <igcz025221@gmail.com>
2025-06-03 16:01:52 +0900

    feat: Strengthsセクションを追加し、Aboutセクションを更新 fix: CSSの文字色・行間・文字間を調整

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Sidebar } from './components/Sidebar';
 import { About } from './components/sections/About';
 import { History } from './components/sections/History';
 import { Skills } from './components/sections/Skills';
+import { Strengths } from './components/sections/Strengths';
 import { Contact } from './components/sections/Contact';
 import type { ContentsType } from './types/contents';
 
@@ -10,16 +11,20 @@ function App() {
         { label: 'About', label_ja: '自己紹介', link: '#about' },
         { label: 'History', label_ja: '経歴', link: '#history' },
         { label: 'Skills', label_ja: 'スキル', link: '#skills' },
+        { label: 'Strengths', label_ja: '強み', link: '#strengths' },
         { label: 'Contact', label_ja: 'お問い合わせ', link: '#contact' },
     ];
     return (
         <>
             <Sidebar contents={contents}></Sidebar>
-            <div className="ml-[300px] p-12">
-                <About content={contents[0]}></About>
-                <History content={contents[1]}></History>
-                <Skills content={contents[2]}></Skills>
-                <Contact content={contents[3]}></Contact>
+            <div className="ml-[300px]">
+                <div className='p-8 max-w-[800px] mx-auto'>
+                    <About content={contents[0]}></About>
+                    <History content={contents[1]}></History>
+                    <Skills content={contents[2]}></Skills>
+                    <Strengths content={contents[3]}></Strengths>
+                    <Contact content={contents[4]}></Contact>
+                </div>
             </div>
         </>
     );
--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -1,13 +1,80 @@
 import type { ContentItemType } from '../../types/contents';
 import { Heading } from '../Heading';
+import { FaArrowAltCircleDown } from 'react-icons/fa';
 
 export function About({ content }: { content: ContentItemType }) {
+    const copyStyle = "block w-fit text-4xl text-gray-50 bg-gray-800 my-4";
+    const tableRowStyle = "border-b border-gray-300";
+    const handleClick = (link: string) => {
+        const el = document.getElementById(link);
+        if (el) {
+            const y = el.getBoundingClientRect().top + window.pageYOffset - 50;
+            window.scrollTo({ top: y, behavior: 'smooth' });
+        }
+    };
     return (
         <section>
             <Heading content={content}></Heading>
-            <p>
-                打ち明けてみようかという疑問に会って静かに話す機会を奪われていた。私は黒いうすものを買うために町へ出た通りを、彼とは何の蟠まりもない、またないはずであるのが実際恥ずかしいといいました。私はそのまま二、三日を費やした。先生は座敷からこの椿の花はもう一つもありませんが、まるで内所話でもする人のようにやり込めるのです。しかもその信用は初対面の時からの仲好でした。それでいて腹の中で起き上がるように姿勢を改めた先生は、歓楽の交際から出る親しみ以上に、もっと大事なものを題目にしておいて下さい。それからお嬢さんより外に先生を解釈する事ができなかったのです。のみならず数ある同級生のうちで思いました。年輩の先生の語気は前と同じような意味の書状が二、三日して、極めて少なくなった。Ｋはそうではないと答えてまた自分の寝床へ帰った私は、そのままで立ち行くだろうか。やったんですかあるというほどの分別は出なかった。私は何だかひっそりしているらしかった。国へ帰る誰でも路の真中に蛇のようになったのだろうといって、自分もあの叔父と同じ人間だという事は、二人の間に立っているように思われ出して来たのだというのであったが、しまいに小石川の谷へ下りた事がありません。私はお嬢さんがすべて私の方が学生らしい学生だったのです。それで二人は同じ病気で死んだというのは横着な了簡だからね私は母に相談しました。
-            </p>
+            <div className='flex content-center justify-center items-center'>
+                <div className='text-2xl font-bold text-gray-800'>Philosophy</div>
+                <div className='mx-15 font-bold'>
+                    <span className={copyStyle}>課題を自分ごと化し、</span>
+                    <span className={copyStyle}>チームと共に価値を</span>
+                    <span className={copyStyle}>創造するエンジニアへ</span>
+                </div>
+            </div>
+            <table className='table-auto w-full border-collapse border-gray-300 mt-8'>
+                <tbody className='border-t border-gray-300'>
+                    <tr className={tableRowStyle}>
+                        <td className='p-3 font-bold'>ニックネーム</td>
+                        <td className='p-3'>きつねうどん（@ktsn-ud）<span className='text-xs'>※受験生時代にたくさん食べたから</span></td>
+                    </tr>
+                    <tr className={tableRowStyle}>
+                        <td className='p-3 font-bold'>所属</td>
+                        <td className='p-3'>千葉大学 理学部 数学・情報数理学科 情報数理学コース</td>
+                    </tr>
+                    <tr className={tableRowStyle}>
+                        <td className='p-3 font-bold'>卒業年度</td>
+                        <td className='p-3'>2027年3月 卒業見込み</td>
+                    </tr>
+                    <tr className={tableRowStyle}>
+                        <td className='p-3 font-bold'>スキル</td>
+                        <td className='p-3 flex'>
+                            <div>Python / React / Tailwind CSS など</div>
+                            <div className='ml-5 flex items-center cursor-pointer hover:opacity-60' onClick={() => handleClick('#skills')}>
+                                <FaArrowAltCircleDown className='inline'/>
+                                <div className='inline-block ml-1'>詳細</div>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr className={tableRowStyle}>
+                        <td className='p-3 font-bold'>現在の関心分野</td>
+                        <td className='p-3'>Webアプリ開発、SaaS系プロダクト</td>
+                    </tr>
+                    <tr className={tableRowStyle}>
+                        <td className='p-3 font-bold'>強み</td>
+                        <td className='p-3 flex items-end'>
+                            <ul>
+                                <li>論理的思考力と課題解決力</li>
+                                <li>高い自走力と学習意欲</li>
+                                <li>チーム貢献力と支援型リーダーシップ</li>
+                            </ul>
+                            <div className='ml-5 flex items-center cursor-pointer hover:opacity-60' onClick={() => handleClick('#strengths')}>
+                                <FaArrowAltCircleDown className='inline'/>
+                                <div className='inline-block ml-1'>詳細</div>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr className={tableRowStyle}>
+                        <td className='p-3 font-bold'>趣味</td>
+                        <td className='p-3'>
+                            アニメを見ること<br />
+                            <span className='text-xs'>※キャラクターの心の動きに共感したり、関係性の変化を楽しむのが好きです。</span>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+
         </section>
     );
 }
