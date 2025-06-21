5e63b0fd38225b9e2a5a4f0782d7af6b03972d18
ktsn_ud <igcz025221@gmail.com>
2025-06-21 22:53:04 +0900

    refactor: スキルコンポーネントのリファクタリングを実施

--- src/components/sections/Skills.tsx
+++ src/components/sections/Skills.tsx
@@ -1,88 +1,146 @@
+import type React from 'react';
 import type { SectionItemType } from '../../types/sections';
 import { Heading } from '../Heading';
-import skillsList from './skills.json';
+import skills from './skills.json';
 import { FaRegCheckSquare } from 'react-icons/fa';
 import { HiOutlineLink } from 'react-icons/hi';
 import { HiOutlineExternalLink } from 'react-icons/hi';
 
-export function Skills({ section }: { section: SectionItemType }) {
-    const skills = skillsList.map((skill, index) => {
-        // 各項目内、リンク部分
-        const links = (
-            linkArr: { label: string; url: string }[],
-            outerIndex: number,
-        ) => {
-            if (!linkArr) return null;
-            return linkArr.reduce<React.ReactNode[]>(
-                (acc, link, innerIndex) => {
-                    if (innerIndex > 0 && window.innerWidth >= 1024) {
-                        acc.push(
-                            <span
-                                key={`sep-${outerIndex}-${innerIndex}`}
-                                className="mx-1 text-gray-400 select-none"
-                            >
-                                ｜
-                            </span>,
-                        );
-                    }
+type LinkType = { label: string; url: string };
+type SkillItemType = {
+    title: string;
+    description: string;
+    links: LinkType[] | null;
+};
+
+function Links({
+    links,
+    parentKey,
+}: {
+    links: LinkType[] | null;
+    parentKey: string;
+}) {
+    let linksElements;
+    if (!links) {
+        linksElements = null;
+    } else {
+        linksElements = links.reduce<React.ReactNode[]>(
+            (acc, link, linkIdx) => {
+                if (linkIdx > 0 && window.innerWidth >= 1024) {
                     acc.push(
-                        <a
-                            href={link.url}
-                            key={`link-${outerIndex}-${innerIndex}`}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="hover:opacity-60 block lg:inline"
+                        <span
+                            key={`skill-link-${parentKey}-sep-${linkIdx}`}
+                            className="mx-1 text-gray-400 select-none"
                         >
-                            {link.label}
-                            <HiOutlineExternalLink className="inline ml-1" />
-                        </a>,
+                            ｜
+                        </span>,
                     );
-                    return acc;
-                },
-                [],
-            );
-        };
-
-        // 各分野内、項目
-        const details = skill.items.map((item, innerIndex) => {
-            return (
-                <ul key={`skill-${index}-${innerIndex}`} className="pl-[2.5em]">
-                    <li className="my-5">
-                        <div className="relative font-bold mr-2">
-                            <FaRegCheckSquare className="absolute -left-[1.5em] top-[3px]" />
-                            {item.title}:
-                        </div>
-                        <div className="lg:pl-[1.5em] leading-6 text-sm">
-                            {item.description}
-                            {item.links && (
-                                <div className="my-1">
-                                    <span className="border inline-block p-1 mr-2">
-                                        <HiOutlineLink className="inline mr-1" />
-                                        関連リンク
-                                    </span>
-                                    {links(item.links, index)}
-                                </div>
-                            )}
-                        </div>
-                    </li>
-                </ul>
-            );
-        });
-
-        return (
-            // 各分野見出し
-            <div key={`skill-field-${index}`} className="my-10">
-                <h3 className="font-bold text-xl pl-3 my-4 border-b-2">
-                    {skill.field}
-                </h3>
-                {details}
-            </div>
+                }
+                acc.push(
+                    <a
+                        href={link.url}
+                        key={`skill-link-${parentKey}-${linkIdx}-${link.label}`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="hover:opacity-60 block lg:inline"
+                    >
+                        {link.label}
+                        <HiOutlineExternalLink className="inline ml-1" />
+                    </a>,
+                );
+                return acc;
+            },
+            [],
         );
-    });
+    }
+
+    return (
+        <div className="my-1">
+            <span className="border inline-block p-1 mr-2">
+                <HiOutlineLink className="inline mr-1" />
+                関連リンク
+            </span>
+            {linksElements}
+        </div>
+    );
+}
+
+function SkillItem({
+    title,
+    description,
+    links,
+    id,
+    parentKey,
+}: {
+    title: string;
+    description: string;
+    links: LinkType[] | null;
+    id: number;
+    parentKey: string;
+}) {
+    return (
+        <ul key={`skill-${parentKey}-${id}`} className="pl-[2.5em]">
+            <li className="my-5" key={`skill-li-${parentKey}-${id}`}>
+                {' '}
+                {/* key追加 */}
+                <div className="relative font-bold mr-2">
+                    <FaRegCheckSquare className="absolute -left-[1.5em] top-[3px]" />
+                    {title}
+                </div>
+                <div className="lg:pl-[1.5em] leading-6 text-sm">
+                    {description}
+                    {links && (
+                        <Links
+                            links={links}
+                            parentKey={`skill-${parentKey}-${id}`}
+                        />
+                    )}
+                </div>
+            </li>
+        </ul>
+    );
+}
+
+function Field({
+    field,
+    items,
+    id,
+}: {
+    field: string;
+    items: SkillItemType[];
+    id: number;
+}) {
+    const skillItemsElements = items.map((item, itemIdx) => (
+        <SkillItem
+            title={item.title}
+            description={item.description}
+            links={item.links}
+            id={itemIdx}
+            parentKey={`skill-field-${id}`}
+            key={`skillitem-${id}-${itemIdx}-${item.title}`}
+        />
+    ));
+    return (
+        <div key={`skill-field-${id}`} className="my-10">
+            <h3 className="font-bold text-xl pl-3 my-4 border-b-2">{field}</h3>
+            {skillItemsElements}
+        </div>
+    );
+}
+
+export function Skills({ section }: { section: SectionItemType }) {
+    const skillElements = skills.map((skill, skillIdx) => (
+        <Field
+            field={skill.field}
+            items={skill.items}
+            id={skillIdx}
+            key={`field-${skillIdx}-${skill.field}`}
+        />
+    ));
     return (
         <section>
-            <Heading section={section}></Heading>
-            {skills}
+            <Heading section={section} />
+            {skillElements}
         </section>
     );
 }
